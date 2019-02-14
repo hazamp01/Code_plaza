dd89a904467f9d0016307c88573a1ee97cc2a6e0
Mohanpraveen <38327559+hazamp01@users.noreply.github.com>
2019-02-13 16:09:54 -0800

    Update imagecalculations.py

--- imagecalculations.py
+++ imagecalculations.py
@@ -9,9 +9,12 @@ min=np.min(X_data)
 max=np.max(X_data)
 mean = np.mean(X_data)
 std=np.std(X_data)
+# General formula for calculating standard deviation
 # std = sqrt(mean(abs(min - max.mean())**2))
-# adjsted = max(std,1/np.sum(np.array(X_data).shape))
-# print('X_data shape:', np.array(X_data).shape)
+# Adjusted standard deviation
+adjsted = max(std,1/np.sum(np.array(X_data).shape))
+print('X_data shape:', np.array(X_data).shape)
+print np.array(X_data).shape
 print 1/np.sum(np.array(X_data).shape)
 print mean
 print std
