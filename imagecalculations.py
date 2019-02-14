e4bec5cca6c28ccd56c966a9ac0bfbc5ef19f7ce
Mohanpraveen <38327559+hazamp01@users.noreply.github.com>
2019-02-13 16:06:57 -0800

    deep_learning_Image_Calculations

---
+++ imagecalculations.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+from math import sqrt
+
+X_data = []
+files = "/Users/mohanpraveenhazaru/Documents/audit_log/gehc-ai-lf-inference-package/test/input/ptx/noptx-2.png"
+X_data.append (cv2.imread (files))
+min=np.min(X_data)
+max=np.max(X_data)
+mean = np.mean(X_data)
+std=np.std(X_data)
+# std = sqrt(mean(abs(min - max.mean())**2))
+# adjsted = max(std,1/np.sum(np.array(X_data).shape))
+# print('X_data shape:', np.array(X_data).shape)
+print 1/np.sum(np.array(X_data).shape)
+print mean
+print std
+print min
+print max
